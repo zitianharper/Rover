6460ece8697504cc049cd24ff95a2d50b30baf75
Zitian Harper <Zitian.harper@icloud.com>
2026-04-13 10:28:41 +0100

    create readme, testfile, git ignore

---
+++ Rover.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Rover.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
